5ce54dfb42f2d2bed620aa7e367b6c2c81c1c92a
Sun Kit Tsui <sunkittsui@gmail.com>
2022-09-10 11:15:10 -0500

    Added methods to the LinkedList interface
    Added the following methods to the LinkedList interface:
    - void add(long index, E element);
    - E get(long index);
    - long size();
    - String toString();

--- src/main/java/datastructures/interfaces/LinkedList.java
+++ src/main/java/datastructures/interfaces/LinkedList.java
@@ -2,5 +2,9 @@ package datastructures.interfaces;
 
 public interface LinkedList<E> {
     void add(E element);
+    void add(long index, E element);
+    E get(long index);
     void remove(long index);
+    long size();
+    String toString();
 }
